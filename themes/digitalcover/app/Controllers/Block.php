f756a0c30492e112a029ea10dea3f6f6090fa624
gregorydigitalcover <g.lalle@digital-cover.fr>
2022-01-27 10:16:41 +0100

    🐣 [START] Initial commit

---
+++ themes/digitalcover/app/Controllers/Block.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use Sober\Controller\Controller;
+
+class Block extends Controller {
+  // public static function example($data) {
+  //   return array(
+  //     'title' => Element::title($data),
+  //     'image' => Element::image($data['image'], '1920px')
+  //   );
+  // }
+}
---
+++ themes/digitalcover/resources/views/partials/grid.blade.php
@@ -0,0 +1,9 @@
+<div class="grid">
+  <div class="container-fluid h-100">
+    <div class="row h-100">
+      @for ($i = 0; $i < 24; $i++)
+        <div class="col-1"><div class="grid__item h-100"></div></div>
+      @endfor
+    </div>
+  </div>
+</div>
