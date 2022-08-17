ec0e8668d38b43dd96fe122116676dd749304fea
rNavas <r.navas@digital-cover.fr>
2022-08-17 10:55:34 +0200

    🔥 [FRONTPAGE] Remove post loop

--- themes/digitalcover/resources/views/front-page.blade.php
+++ themes/digitalcover/resources/views/front-page.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-  @while (have_posts()) @php the_post() @endphp
-    <div data-router-view="page">
-      <div class="home">
-        {!! the_content() !!}
-      </div>
+  <div data-router-view="page">
+    <div class="home">
+      {!! the_content() !!}
     </div>
-  @endwhile
+  </div>
 @endsection
