fb525723badf1f718edfc113637f036f4b7ad2c4
Matth-Ben <matthias.benoit@outlook.fr>
2023-01-09 15:32:48 +0100

    🎆 [ARCHIVE] Template + skin + RWD

--- themes/digitalcover/resources/views/components/card-post.blade.php
+++ themes/digitalcover/resources/views/components/card-post.blade.php
@@ -5,12 +5,12 @@
 <a href="{{ get_the_permalink($id) }}" class="card-post">
   <div class="card-post__thumbnail">
     @include('elements/image', ['data' => $data['image']])
-  </div>
-  <div class="card-post__body">
     <div class="card-post__title">
       {{ get_the_title($id) }}
     </div>
+  </div>
+  <div class="card-post__body">
     <div class="card-post__excerpt">{{ get_the_excerpt($id) }}</div>
-    <div class="card-post__button button">En savoir plus</div>
+    <div class="card-post__button">En savoir plus</div>
   </div>
 </a>
--- themes/digitalcover/resources/views/index.blade.php
+++ themes/digitalcover/resources/views/index.blade.php
@@ -1,17 +1,24 @@
 @extends('layouts.app')
 
+@php
+  $field = get_fields(get_queried_object_id());
+  $data = Block::cover($block['data'] = $field);
+  $categories = get_categories();
+  $zones = get_terms(['taxonomy' => 'zones']);
+@endphp
+
 @section('content')
   <div data-router-view="page">
     <section class="news">
       <div class="news-head">
-        {{-- @include('blocks/cover-slider', ['data' => $data]) --}}
+        @include('blocks/cover', ['data' => $data])
       </div>
       <div class="container-fluid">
         <div class="row">
           <div class="col-22 col-lg-15 offset-1">
             <div class="news-body u-margin">
+              <div class="news-filter"></div>
               <div class="news-posts">
-                {{-- {!! $content !!} --}}
                 @while(have_posts()) @php the_post() @endphp
                   <div class="subcol-lg-5">
                     @include('components/card-post', ['id' => get_the_ID()])
@@ -29,10 +36,10 @@
       </div>
 
       <div class="news-pagination">
-        {{-- {!! posts_nav_link(' ',
+        {!! posts_nav_link(' ',
           '<span class="prev">',
           '<span class="next">'
-        ) !!} --}}
+        ) !!}
       </div>
     </div>
   </div>
